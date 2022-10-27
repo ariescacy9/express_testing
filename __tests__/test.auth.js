bb48e687909059aae330528d65adec640fe9208d
tristan99 <ariescacy9@gmail.com>
2022-10-27 11:47:21 -0500

    change version

--- __tests__/test.auth.js
+++ __tests__/test.auth.js
@@ -26,42 +26,6 @@ describe('Test the auth endpoints', () => {
     expect(user.last_name).to.equal(payload.last_name);
   });
 
-  /*
-  it('should return 400 if payload is incomplete', async () => {
-    const payload = {
-      'name': 'teast',
-      'email': 'josae@email.com'
-    }
-    const { status } = await request(app).post('/auth/register').send(payload);
-    expect(status).to.equal(400);
-  });
-*/
-/*
-  it('should return 400 if password is shorter than 6 characters', async () => {
-    const payload = {
-      'name': 'teast',
-      'last_name': 'test',
-      'email': 'josa123e@email.com',
-      'password': '12345'
-    }
-    const { body, status } = await request(app).post('/auth/register').send(payload);
-    expect(status).to.equal(400);
-    expect(body.message).contains('password must be at least 6 characters');
-  });
-  */
-/*
-  it('should return 400 if email does not have an @ character', async () => {
-    const payload = {
-      'name': 'teast',
-      'last_name': 'test',
-      'email': 'josa123e.email.com',
-      'password': '12345123123'
-    }
-    const { body, status } = await request(app).post('/auth/register').send(payload);
-    expect(status).to.equal(400);
-    expect(body.message).contains('@');
-  });
-*/
   it('should allow to login', async () => {
     const payload = {
       'email': 'josae@email.com',
