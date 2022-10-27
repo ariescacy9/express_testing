2fae3ca415478315d5474dcd5741be48748637af
tristan99 <ariescacy9@gmail.com>
2022-10-27 14:25:58 -0500

    change database config

--- config/database.js
+++ config/database.js
@@ -7,13 +7,14 @@ const env = process.env.ENV;
 const config = _config[env];
 
 const db = new Sequelize(
-  process.env.DB_NAME, //esta base de dataos ya la tenemos creadas "larnu_demo"
-  process.env.DB_USER, //"larnu"
-  process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST, //si trabajamos en nuestra computadora solamente
-  dialect: 'postgres', //dialecto
-  logging: false,
-  port: process.env.DB_PORT, //es el puerto por defecto, si tenemos otro tenemos que ponerlo, sino dejamos este
+  config.database, //esta base de dataos ya la tenemos creadas "larnu_demo"
+  config.username, //"larnu"
+  config.password, 
+  {
+    host: config.host, //si trabajamos en nuestra computadora solamente
+    dialect: 'postgres', //dialecto
+    logging: false,
+    port: 5432, //es el puerto por defecto, si tenemos otro tenemos que ponerlo, sino dejamos este
   define: {
     timestamps: false //tiene el uso de dos campos createup y updatecreate, nosotros aca ledesimos que no haga eso
   }
