995159362f1fad2c1d24b355e0f0bd648907000c
tristan99 <ariescacy9@gmail.com>
2022-11-03 09:10:53 -0500

    change in config.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,12 @@
+/*
 import { Sequelize } from "sequelize";
-/*import * as dotenv from 'dotenv';
-dotenv.config();
-*/
 import _config from "../config/config.js"
 const env = process.env.ENV;
 const config = _config[env];
 
 const db = new Sequelize(
-  config.database, //esta base de dataos ya la tenemos creadas "larnu_demo"
-  config.username, //"larnu"
+  config.database, //mi base de datos se llama postgres tambien
+  config.username, //"postgres"
   config.password, 
   {
     host: config.host, //si trabajamos en nuestra computadora solamente
@@ -19,5 +17,23 @@ const db = new Sequelize(
     timestamps: false //tiene el uso de dos campos createup y updatecreate, nosotros aca ledesimos que no haga eso
   }
 });
+
+export default db;
+*/
+import { Sequelize } from "sequelize";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const db = new Sequelize(
+  process.env.DB_NAME || 'postgres',
+  process.env.DB_USER || 'postgres',
+  process.env.DB_PASSWORD || '1234567890',
+  {
+    host: process.env.DB_HOST|| 'localhost',
+    port: process.env.DB_PORT|| 5432,
+    dialect: "postgres",
+    logging: false 
+  }
+);
 
 export default db;
